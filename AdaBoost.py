2b22f6d42c66abe2cf91eaa60c2d0bee102a501e
Roi Abramovitch <roiabr15@gmai.com>
2020-06-03 14:41:55 +0300

    working with the file

---
+++ AdaBoost.py
@@ -0,0 +1,12 @@
+import numpy as np
+from itertools import combinations
+
+file = np.loadtxt('C:/Users/Roi Abramovitch/Downloads/HC_Body_Temperature')
+
+file = np.where(file == 2, -1, file)
+
+x1 = np.array(file[:, 0])
+x2 = np.array(file[:, 2])
+y = np.array(file[:, 1])
+X = np.vstack((x1, x2)).T
+print(X)

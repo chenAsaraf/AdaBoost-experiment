813eb24db7703c39321289178e40908c2af4950f
Roi Abramovitch <roiabr15@gmai.com>
2020-06-03 23:45:16 +0300

    working on Adaboost

--- AdaBoost.py
+++ AdaBoost.py
@@ -1,14 +1,36 @@
+import math
+
 import numpy as np
-from itertools import combinations
+import Rectangles
+def adaBoost():
+    # Initialization of utility variables
+    path = 'C:/Users/Roi Abramovitch/Downloads/HC_Body_Temperature'
+    features, labels, weights = Rectangles.read_data(path)
 
-file = np.loadtxt('C:/Users/Roi Abramovitch/Downloads/HC_Body_Temperature')
+    # For r = 1 to r
+    for m in range(2):
+        for i in range(100):
+            rec_ht = Rectangles.rectangle(features, labels, weights)
+            print(rec_ht[1])
+            new_weights = 0.5 * np.log((1.0 - rec_ht[1])/rec_ht[1])
+            print(new_weights)
+            for point in range(features.shape[0]):
+                if update_weights(rec_ht[0],features[point]) != labels[point]:
+                    weights[point] = weights[point] * np.exp(-new_weights)
+                else:
+                    weights[point] = weights[point] * np.exp(new_weights)
+            weights /= np.sum(weights)
 
-file = np.where(file == 2, -1, file)
 
-x1 = np.array(file[:, 0])
-x2 = np.array(file[:, 2])
-y = np.array(file[:, 1])
-X = np.vstack((x1, x2)).T
-com = combinations(X,2)
-for i in list(com):
-    print(i[0][1])
+def update_weights(rectangle, features):
+    # if y-coordinate is between the y's coordinates of the rectangle:
+    # print(point)
+    # print("self.upperLeft[0]", self.upperLeft[0], "self.bottomLeft[0]", self.bottomLeft[0])
+    if features[1] < rectangle.upperLeft[1] and features[1] > rectangle.bottomRight[1]:
+        # between x-coordinates:
+        if features[0] > rectangle.upperLeft[0] and features[0] < rectangle.bottomRight[0]:
+            return 1
+    return -1
+
+if __name__ == '__main__':
+    adaBoost()

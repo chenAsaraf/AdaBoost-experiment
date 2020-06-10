b03f0ebc52029fffa256ad0e7fac31bedfe7b1f9
Roi Abramovitch <roiabr15@gmai.com>
2020-06-10 16:05:52 +0300

    print the average error of test and train

--- AdaBoost.py
+++ AdaBoost.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import random
 from Rectangles import Rectangle
+from collections import defaultdict
 
 
 def read_data(path):
@@ -27,40 +28,68 @@ def splitToFeaturesLabels(data):
     return X, Y
 
 
-def adaBoost(numberOfModels):
-    # Initialization of utility variables
-    path = 'C:/Users/owner/Desktop/dataset.txt'
-    data = read_data(path)
-    dataPointsNumber = data.shape[0]
+def adaBoost(trainFeatures, trainLabels, numberOfModels):
 
+    dataPointsNumber = trainFeatures.shape[0]
+    # Initialize each point weight to be 1 / n:
     weights = np.empty(dataPointsNumber)
     weights.fill(1 / dataPointsNumber)
 
-    # final results:
+    # final results containers::
     hypothesis = []
+    alphas = []
 
     for i in range(numberOfModels):
-        train, test = splitTestTrain(data)
-        # print(train)
-        trainFeatures, trainLabels = splitToFeaturesLabels(train)
-        # print(trainFeatures , trainLabels)
-        bestRectangle, minError = Rectangle(trainFeatures, trainLabels, weights)
-        print("minError: ", minError)
-        new_weights = float(0.5 * np.log((1.0 - minError) / minError))
-        print("new_weights: ", new_weights)
-        print("------")
+
+        # Using Rectangle to find a rectangle with minimum weighted error εt
+        rectangle_t, error_t = Rectangle(trainFeatures, trainLabels, weights)
+        # Rectangle alpha (the weight):
+        alpha_t = float(0.5 * np.log((1.0 - error_t) / error_t))
+        #print("alpha_t: ", alpha_t)
+        #print("------")
+        # Compute new weights for the points:
         for point in range(trainFeatures.shape[0]):
-            if bestRectangle.h(trainFeatures[point]) != trainLabels[point]:
-                # if update_weights(bestRectangle,features[point]) != labels[point]:
-                weights[point] = weights[point] * np.exp(-new_weights)
-            else:
-                weights[point] = weights[point] * np.exp(new_weights)
+            # For an error on point:
+            if rectangle_t.h(trainFeatures[point]) != trainLabels[point]:
+                weights[point] = weights[point] * np.exp(-alpha_t)
+            else: # Not an error on point:
+                weights[point] = weights[point] * np.exp(alpha_t)
+        # Normalize these weights:
         weights /= np.sum(weights)
-    hypothesis.append(bestRectangle)
+        hypothesis.append(rectangle_t)
+        alphas.append(alpha_t)
+
+    return hypothesis, alphas
 
 
 if __name__ == '__main__':
-    r = 8
-    for t in range(1, r):
-        for i in range(10):
-            adaBoost(t)
+
+    path = 'C:/Users/Roi Abramovitch/Downloads/לימודים מדעי המחשב/שנה ג/למידת מכונה/מטלות/מטלה 2/HC_Body_Temperature'
+    data = read_data(path)
+    dataPointsNumber = data.shape[0]
+
+    R = 9
+    iteration = 10
+    for r in range(1, R):
+        errorTest = 0.0
+        errorTrain = 0.0
+        for i in range(iteration):
+            train, test = splitTestTrain(data)
+            trainFeatures, trainLabels = splitToFeaturesLabels(train)
+            testFeatures, testLabels = splitToFeaturesLabels(test)
+            H, alphas = adaBoost(trainFeatures, trainLabels, r)
+
+            for point in range(testFeatures.shape[0]):
+                sign = 0
+                for index_t in range(len(H)):
+                    sign += alphas[index_t] * H[index_t].h(testFeatures[point])
+                errorTest += int(sign * testLabels[point] < 0)
+            for point in range(trainFeatures.shape[0]):
+                sign = 0
+                for index_t in range(len(H)):
+                    sign += alphas[index_t] * H[index_t].h(trainFeatures[point])
+                errorTrain += int(sign * trainLabels[point] < 0)
+        print('r:', r, 'average error:',  (errorTest/ iteration)/65)
+        print('r:', r, 'average error:', (errorTrain / iteration) / 65)
+
+

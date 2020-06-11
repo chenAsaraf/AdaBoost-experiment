78f470c5983997bc205da0a3b321e4d3624f8e37
Roi Abramovitch <44756354+Roiabr@users.noreply.github.com>
2020-06-11 12:01:14 +0300

    Update Rectangles.py

--- Rectangles.py
+++ Rectangles.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as plt
 
 """
  Set of axis-parallel rectangles
- for which the inside is positive and the outside is negative.
- Rectangle can c by the 2 points on its diagonal.
+ for which the inside is *positive* and the outside is negative.
+ Rectangle can defined by the 2 points on its diagonal.
  """
+
+
 class RectangleModel:
     """
     Input:
@@ -16,17 +18,13 @@ class RectangleModel:
                 * X the points - nx2 matrix
                 * y the labels of the points
                 * weights of the points
-     Note: This implementation based on assumption that
-            the points whose real label is positive stored sequentially
-            from the beginning of the data-list.
     """
 
-    def __init__(self, firstPoint, secondPoint, X, y, w):
+    def _init_(self, firstPoint, secondPoint, X, y, w):
         self.data = X
         self.labels = y
         self.weights = w
         self.__initPoints(firstPoint, secondPoint)
-
 
     # initialize rectangle borders
     def __initPoints(self, firstPoint, secondPoint):
@@ -37,7 +35,7 @@ class RectangleModel:
         if firstPoint[0] <= secondPoint[0]:  # if x coordinate is smaller in first point
             right = secondPoint[0]
             left = firstPoint[0]
-        elif firstPoint[0] > secondPoint[0]:
+        if firstPoint[0] > secondPoint[0]:
             right = firstPoint[0]
             left = secondPoint[0]
         if firstPoint[1] <= secondPoint[1]:  # if y coordinate is smaller in first point
@@ -48,9 +46,8 @@ class RectangleModel:
             down = secondPoint[1]
         self.upperLeft = [left, up]
         self.upperRight = [right, up]
-        self.bottomRight = [right,down]
+        self.bottomRight = [right, down]
         self.bottomLeft = [left, down]
-
 
     # Returns the lable that the rectangle gives to the point
     def h(self, point):
@@ -77,15 +74,14 @@ class RectangleModel:
         fig = plt.gcf()
         ax = fig.gca()
         for i in range(self.data.shape[0]):
-            if self.labels[i] == 1: # Positive points:
+            if self.labels[i] == 1:  # Positive points:
                 plt.scatter(self.data[i, 0], self.data[i, 1], color="green", s=30)
-            else:# Negative points:
+            else:  # Negative points:
                 plt.scatter(self.data[i, 0], self.data[i, 1], color="blue", s=30)
         ax.grid()
         # Create a Rectangle patch
         width = self.bottomRight[0] - self.bottomLeft[0]
         height = self.upperLeft[1] - self.bottomLeft[1]
-        print("*****", self.bottomLeft)
         rect = patches.Rectangle(self.bottomLeft, width, height, linewidth=3, edgecolor='r', facecolor='none')
         # Add the patch to the Axes
         ax.add_patch(rect)
@@ -96,10 +92,16 @@ class RectangleModel:
         plt.show()
 
 
+"""
+ Set of axis-parallel rectangles
+ for which the inside is *negative* and the outside is positive.
+ Rectangle can defined by the 2 points on its diagonal.
+ """
+
 
 class NegativeRectangleModel(RectangleModel):
-    def __init__(self, firstPoint, secondPoint, X, y, w):
-        super(NegativeRectangleModel, self).__init__(firstPoint, secondPoint, X, y, w)
+    def _init_(self, firstPoint, secondPoint, X, y, w):
+        super(NegativeRectangleModel, self)._init_(firstPoint, secondPoint, X, y, w)
 
     # Returns the lable that the rectangle gives to the point
     def h(self, point):
@@ -115,15 +117,18 @@ class NegativeRectangleModel(RectangleModel):
 Rectangle function:
     given: * a set of labelled and weighted points,
            * and the hypothesis class of all axis-parallel rectangles for which
-            the inside is positive and the outside is negative,
+            the inside is positive and the outside is negative, or the opposite,
     finds the rectangle which minimizes the weighted error on the points.
 Algo:
     1. Look for any possible rectangle:
-            by any possible combination of two points labeled positive,
-            which define the diagonal of the rectangle.
+            by any possible combination of two points,
+            which define the diagonal of the rectangle,
+            whether within the rectangle is defined as positive or vice versa.
     2. For each rectangle compute the weighted error
     3. Choose the one that has the smallest error  
 """
+
+
 def Rectangle(X, y, weights):
     minError = float('inf')
     bestRectangle = []
@@ -143,6 +148,4 @@ def Rectangle(X, y, weights):
         if minError > posError:
             minError = posError
             bestRectangle = posRectangle
-    #bestRectangle.draw()
     return bestRectangle, minError
-
